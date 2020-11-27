eb927a4c0f6a3ded80e43f9b363819304a230659
Bianca M Davey <60249994+bianca534@users.noreply.github.com>
2020-11-27 16:04:12 +0100

    Create movie-database.sql

---
+++ movie-database.sql
@@ -0,0 +1,38 @@
+-- Database for MovieDirect
+
+CREATE TABLE Customers (
+customer_id INT NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+first_name VARCHAR(50) NOT NULL,
+address VARCHAR(200),
+city VARCHAR(50),
+state VARCHAR(3) CHECK(state IN ('NSW', 'QLD', 'VIC', 'ACT', 'TAS', 'NT', 'SA', 'WA')), 
+postcode VARCHAR(8),
+PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE Movies (
+movie_id INT NOT NULL,
+movie_title VARCHAR(100) NOT NULL,
+director_last_name VARCHAR(50) NOT NULL,
+director_first_name VARCHAR(50) NOT NULL,
+genre VARCHAR(20) CHECK(genre IN ('Action', 'Adventure', 'Comedy', 'Romance', 'Science Fiction', 'Documentary', 'Drama', 'Horror')),
+media_type VARCHAR(20) CHECK(media_type IN ('DVD', 'Blu-Ray')),
+release_date DATE,
+studio_name VARCHAR(50),
+retail_price REAL CHECK(retail_price > 0),
+current_stock INT CHECK(current_stock >= 0),
+PRIMARY KEY (movie_id)
+);
+
+CREATE TABLE Shipments (
+shipment_id INT NOT NULL,
+customer_id INT NOT NULL,
+movie_id INT NOT NULL,
+shipment_date DATE,
+PRIMARY KEY(shipment_id),
+FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+	ON UPDATE CASCADE,
+FOREIGN KEY(movie_id) REFERENCES Movies(movie_id)
+	ON UPDATE CASCADE
+);
